bf176d0bc38e568cb746c16b836a55d9257dcfab
Stephen Beckstrand <stephen@ivoryimage.com>
2016-07-11 10:52:19 -0400

    First commit

---
+++ Variable Challenge/js/script.js
@@ -0,0 +1,7 @@
+var noun = prompt("Please provide a noun");
+var verb = prompt("Please provide a verb");
+var adjective = prompt("Please provide an adjective");
+
+alert("No more input needed, you're finished!");
+
+document.write("Once upon a time there was a " + adjective + " " + noun + " that " + verb + "." );
