69d5c89ebcd355ed9cef73858fd791047eada0bd
Stephen Beckstrand <stephen@localhost.localdomain>
2016-07-24 18:47:53 -0600

    Start of new Quiz Project with jQuery

--- Variable Challenge/js/script.js
+++
@@ -1,7 +0,0 @@
-var noun = prompt("Please provide a noun");
-var verb = prompt("Please provide a verb");
-var adjective = prompt("Please provide an adjective");
-
-alert("No more input needed, you're finished!");
-
-document.write("Once upon a time there was a " + adjective + " " + noun + " that " + verb + "." );
