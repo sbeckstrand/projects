2940c19e699f406c904ce588e401bd0eb9863f60
Stephen Beckstrand <spbeckstrand@gmail.com>
2020-07-28 06:38:23 -0600

    Deleted charity run form for real this time

--- runForm/js/script.js
+++
@@ -1,5 +0,0 @@
-var participant = "<form><div class='form-group'><input type='text' class='form-control' placeholder='John'><input type='text' class='form-control' placeholder='Doe'></div></form>"
-
-$(".addParticipant").on( "click", function() {
-  console.log("testing");
-});
